315cc12197cbb42005e56f848ca0207eb6f31ef9
Shuhei Gotou <uhsog.tsukuba@gmail.com>
2016-08-02 00:47:01 +0000

    Add user microposts

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -35,7 +35,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", login_path, count:0
-    assert_select "a[href=?]", signup_path
+    #assert_select "a[href=?]", signup_path
     assert_select "a[href=?]", users_path
     assert_select "a[href=?]", edit_user_path(@user)
     assert_select "a[href=?]", logout_path
